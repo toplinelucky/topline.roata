34389a0f2a5bebbd43ad0a4d7e1e5a01206c7309
toplinelucky <topline.lucky@gmail.com>
2026-02-11 16:46:11 +0200

    Update wheel.js

--- wheel.js
+++ wheel.js
@@ -32,7 +32,7 @@ const WHEEL_OFFSET_DEG = 0;
 
 // ✅ DOAR VIZUAL: muti textul stanga/dreapta fara sa afecteze premiul
 // valori bune: -0.12 .. +0.12
-const TEXT_VISUAL_OFFSET_RAD = 0.40;
+const TEXT_VISUAL_OFFSET_RAD = 0.45;
 
 const wheelImg = new Image();
 wheelImg.src = "wheel-base.png";
@@ -242,4 +242,5 @@ spinBtn.addEventListener("click", async () => {
 
   requestAnimationFrame(animate);
 });
+
 
